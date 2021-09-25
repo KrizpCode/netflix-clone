09afb876abd1789183dc6790bc95a58463dab821
KrizpCode <93johan.e@gmail.com>
2021-09-25 19:57:17 +0200

    added movie trailers onClick

--- components/Row/Row.tsx
+++ components/Row/Row.tsx
@@ -1,6 +1,8 @@
 import React, { useState, useEffect } from 'react';
 import axiosInstance from '../../src/axios';
 import Image from 'next/image';
+import YouTube from 'react-youtube';
+import movieTrailer from 'movie-trailer';
 
 const base_url = 'https://image.tmdb.org/t/p/original/';
 
@@ -15,6 +17,9 @@ interface MovieType {
 	name: string;
 	poster_path: string;
 	backdrop_path: string;
+	original_name: string;
+	title: string;
+	overview: string;
 }
 
 interface DataMovieType {
@@ -23,6 +28,7 @@ interface DataMovieType {
 
 const Row: React.FC<Props> = ({ title, fetchUrl, isLargeRow }) => {
 	const [movies, setMovies] = useState<MovieType[] | []>([]);
+	const [trailerUrl, setTrailerUrl] = useState('');
 
 	useEffect(() => {
 		const fetchData = async () => {
@@ -33,6 +39,30 @@ const Row: React.FC<Props> = ({ title, fetchUrl, isLargeRow }) => {
 		fetchData();
 	}, [fetchUrl]);
 
+	const opts = {
+		height: '390',
+		width: '640',
+		playerVars: {
+			autoplay: 1,
+		},
+	};
+
+	const handleClick = (movie: MovieType) => {
+		if (trailerUrl) {
+			setTrailerUrl('');
+		} else {
+			movieTrailer(movie?.title || movie?.name || movie?.original_name || '')
+				.then((url: string) => {
+					const urlParams = new URLSearchParams(new URL(url).search);
+					const trailerUrlParam = urlParams.get('v');
+					if (trailerUrlParam !== null) {
+						setTrailerUrl(trailerUrlParam);
+					}
+				})
+				.catch((error: Error) => console.log(error));
+		}
+	};
+
 	return (
 		<div className="row">
 			<h2>{title}</h2>
@@ -41,6 +71,7 @@ const Row: React.FC<Props> = ({ title, fetchUrl, isLargeRow }) => {
 				{movies.map((movie) => (
 					<img
 						key={movie.id}
+						onClick={() => handleClick(movie)}
 						className={`row__poster  ${isLargeRow && 'row__posterLarge'}`}
 						src={`${base_url}${
 							isLargeRow ? movie.poster_path : movie.backdrop_path
@@ -49,6 +80,7 @@ const Row: React.FC<Props> = ({ title, fetchUrl, isLargeRow }) => {
 					/>
 				))}
 			</div>
+			{trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
 		</div>
 	);
 };
