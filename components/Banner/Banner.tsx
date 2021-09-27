b5527fc7f767fe6d33bdf79603218a1c252b9765
KrizpCode <93johan.e@gmail.com>
2021-09-27 15:35:49 +0200

    Style improvements

--- components/Banner/Banner.tsx
+++ components/Banner/Banner.tsx
@@ -32,9 +32,7 @@ const Banner = () => {
 			className="banner"
 			style={{
 				backgroundSize: 'cover',
-				backgroundImage: `url(
-                    'https://image.tmdb.org/t/p/original/${movie?.backdrop_path}'
-                )`,
+				backgroundImage: `linear-gradient(rgba(0,0,0,0.3), rgba(0,0,0,0.3)), url('https://image.tmdb.org/t/p/original/${movie?.backdrop_path}')`,
 				backgroundPosition: 'center center',
 			}}
 		>
